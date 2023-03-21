905dba261ebf632bbab87ddaf2e16dbbea570a76
Jonathan Herrera <msjona28@outlook.com>
2023-03-20 20:07:28 -0700

    Ejemplo básico con certificado SSL y llave privada (para HTTPs) funcionando

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+// Configuración express
 const puerto = 2800;
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', function(request, response) {
-    response.send('Hello world!');
+// Configuración certificado SSL y llave privada
+// del servidor
+const fs = require('fs');
+const https = require('https');
+const options = { 
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}
+const server = https.createServer(options, app);
+
+app.get('/', (request, response) => {
+    let html = "<h1>Hello world!</h1> <p>Este servidor cuenta con protocolo HTTPs</p>";
+    response.send(html);
 })
 
-app.listen(puerto, function() {
+server.listen(puerto, () => {
+    console.log('Iniciando el servidor HTTPs...');
     console.log(`Escuchando el puerto ${puerto}`);
 });
